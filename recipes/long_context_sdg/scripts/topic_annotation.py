2f15b9ab7d411b579bb5bedd35d3bc724652ab47
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-03 07:57:46 -0800

    changes for the HSG cluster

--- recipes/long_context_sdg/scripts/topic_annotation.py
+++ recipes/long_context_sdg/scripts/topic_annotation.py
@@ -1,6 +1,6 @@
 from nemo_skills.pipeline.cli import generate, wrap_arguments
 
-cluster = "eos"
+cluster = "hsg"
 input_file = "/nemo_run/code/recipes/long_context_sdg/src/test.jsonl"
 output_dir = "/workspace/DATA/LONG_CONTEXT/ANNOTATED_TOPIC_TEST"
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/annotate_document_topic.yaml"
@@ -20,12 +20,12 @@ generate(
     cluster=cluster,
     input_file=input_file,
     output_dir=output_dir,
-    model="/hf_models/gpt-oss-120b",
+    model="/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507",
     server_type="vllm",
     postprocess_cmd=f"python /nemo_run/code/recipes/long_context_sdg/scripts/postprocess_domain_gens.py {output_dir}/output.jsonl {output_dir}/annotated_topics.jsonl",
     # Server parameters
     # num_chunks=10,
-    server_gpus=8,
+    server_gpus=4,
     server_nodes=1,
     server_args="--async-scheduling"
 )
