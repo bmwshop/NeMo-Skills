bf7734fcc3b438cbf94c963f749bf72ee24860fd
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-03 10:32:57 -0800

    sglang instead of vllm

--- recipes/long_context_sdg/scripts/topic_annotation.py
+++ recipes/long_context_sdg/scripts/topic_annotation.py
@@ -6,13 +6,14 @@ input_file = "/workspace/DATA/LONG_CONTEXT/ANNOTATED_TOPIC_TEST/lcr.jsonl"
 output_dir = "/workspace/DATA/LONG_CONTEXT/ANNOTATED_TOPIC_TEST"
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/annotate_document_topic.yaml"
 
+#  f"++max_concurrent_requests=512 "
+#  server_type="vllm",
 
 generate(
     ctx=wrap_arguments(
         f"++skip_filled=True "
         f"++prompt_config={prompt_config} "
         f"++chat_template_kwargs.reasoning_effort=high "
-        f"++max_concurrent_requests=512 "
         f"++inference.tokens_to_generate=8000 "
         f"++inference.endpoint_type=text "
         f"++inference.temperature=1.0 "
@@ -22,11 +23,12 @@ generate(
     input_file=input_file,
     output_dir=output_dir,
     model="/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507",
-    server_type="vllm",
+    server_type="sglang",
+    server_args="--context-len 262144 --ep-size 8",
     postprocess_cmd=f"python /nemo_run/code/recipes/long_context_sdg/scripts/postprocess_domain_gens.py {output_dir}/output.jsonl {output_dir}/annotated_topics.jsonl",
     # Server parameters
     # num_chunks=10,
     server_gpus=4,
-    server_nodes=1,
+    server_nodes=4,
     server_args="--async-scheduling"
 )
