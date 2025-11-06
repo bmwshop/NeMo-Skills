68cdf44969d39a986465c583e814eb95dd0233fb
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-05 19:29:50 -0800

    adjusting the num chunks

--- recipes/long_context_sdg/scripts/qagen.py
+++ recipes/long_context_sdg/scripts/qagen.py
@@ -2,7 +2,7 @@ from nemo_skills.pipeline.cli import generate, wrap_arguments
 
 cluster = "hsg"
 # input_file = "/nemo_run/code/recipes/long_context_sdg/src/test.jsonl"
-input_file = "/workspace/DATA/lc/lcrdocq_topic_annotation/samples_50k.jsonl"
+input_file = "/workspace/DATA/lc/lcrdocq_topic_annotation/samples_10k.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen"
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_qa.yaml"
 # teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
