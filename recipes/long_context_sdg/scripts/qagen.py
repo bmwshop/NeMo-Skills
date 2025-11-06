45892b1685b3550412104aca6aa0be1e96633e6c
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-05 19:01:08 -0800

    qa generation

---
+++ recipes/long_context_sdg/scripts/qagen.py
@@ -0,0 +1,38 @@
+from nemo_skills.pipeline.cli import generate, wrap_arguments
+
+cluster = "hsg"
+# input_file = "/nemo_run/code/recipes/long_context_sdg/src/test.jsonl"
+input_file = "/workspace/DATA/lc/lcrdocq_topic_annotation/samples_50k.jsonl"
+output_dir = "/workspace/DATA/lc/lcrdocq_topic_annotation"
+prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_qa.yaml"
+# teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
+teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Thinking-2507"
+
+#  f"++max_concurrent_requests=512 "
+#  server_type="vllm",
+
+generate(
+    ctx=wrap_arguments(
+        f"++skip_filled=True "
+        f"++prompt_config={prompt_config} "
+        f"++chat_template_kwargs.reasoning_effort=high "
+        f"++inference.tokens_to_generate=16384 "
+        f"++inference.endpoint_type=text "
+        f"++max_concurrent_requests=8 "
+        f"++inference.temperature=0.3 "
+        f"++inference.top_p=0.9 "
+    ),
+    cluster=cluster,
+    input_file=input_file,
+    output_dir=output_dir,
+    model=teacher_model,
+    server_type="sglang",
+    server_args="--context-len 262144 --ep-size 8",
+    # Server parameters
+    num_chunks=16,
+    server_gpus=4,
+    server_nodes=2,
+)
+
+# server_args="--async-scheduling"
+# postprocess_cmd=f"python /nemo_run/code/recipes/long_context_sdg/scripts/postprocess_domain_gens.py {output_dir}/output.jsonl {output_dir}/annotated_topics.jsonl",
