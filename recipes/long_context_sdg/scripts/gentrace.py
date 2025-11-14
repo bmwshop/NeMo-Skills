526b6399ae589b71b501c5933895b56369058b16
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-13 22:24:09 -0800

    more traces

--- recipes/long_context_sdg/scripts/gentrace.py
+++ recipes/long_context_sdg/scripts/gentrace.py
@@ -11,8 +11,8 @@ output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces"
 input_file = "/workspace/DATA/lc/lcrqagen_v2/Government_Consultations-16384-130000_qa.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen_v2/traces"
 
-input_file = "/workspace/DATA/lc/lcrqagen_l_v2/intermediate_p.jsonl"
-output_dir = "/workspace/DATA/lc/lcrqagen_l_v2/traces"
+input_file = "/workspace/DATA/lc/lcrqagen_m_v2/intermediate_p.jsonl"
+output_dir = "/workspace/DATA/lc/lcrqagen_m_v2/traces"
 
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_trace.yaml"
 # teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
