5939e4f631150595126d6feb5208dc6e99ea6acc
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-14 10:52:15 -0800

    more traces

--- recipes/long_context_sdg/scripts/gentrace.py
+++ recipes/long_context_sdg/scripts/gentrace.py
@@ -8,17 +8,17 @@ cluster = "hsg"
 input_file = "/workspace/DATA/lc/lcrqagen_v3/g/76/intermediate_p.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces"
 
-input_file = "/workspace/DATA/lc/lcrqagen_v2/Government_Consultations-16384-130000_qa.jsonl"
-output_dir = "/workspace/DATA/lc/lcrqagen_v2/traces"
+# input_file = "/workspace/DATA/lc/lcrqagen_v2/Government_Consultations-16384-130000_qa.jsonl"
+# output_dir = "/workspace/DATA/lc/lcrqagen_v2/traces"
 
-input_file = "/workspace/DATA/lc/lcrqagen_m_v2/intermediate_p.jsonl"
-output_dir = "/workspace/DATA/lc/lcrqagen_m_v2/traces"
+# input_file = "/workspace/DATA/lc/lcrqagen_m_v2/intermediate_p.jsonl"
+# output_dir = "/workspace/DATA/lc/lcrqagen_m_v2/traces"
 
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_trace.yaml"
 # teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
 teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Thinking-2507"
 
-dependent_jobs = 1
+dependent_jobs = 0
 num_servers = 32
 
 #  f"++max_concurrent_requests=512 "
