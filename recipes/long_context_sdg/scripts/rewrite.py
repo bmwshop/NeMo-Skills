7750b97e08dd78817ff36842b3b2ab014d1ffd5c
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-16 15:31:59 -0800

    more rewrites

--- recipes/long_context_sdg/scripts/rewrite.py
+++ recipes/long_context_sdg/scripts/rewrite.py
@@ -9,8 +9,12 @@ output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/mf/traces_b/rewrites"
 input_file = "/workspace/DATA/lc/lcrqagen_v3/g/mf/traces/critiques/critique.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/mf/traces/rewrites"
 
-input_file = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces/critiques/critique.jsonl"
-output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces/rewrites"
+# input_file = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces/critiques/critique.jsonl"
+# output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces/rewrites"
+
+input_file = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces_b/critiques/critique.jsonl"
+output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/76/traces_b/rewrites"
+
 
 # input_file = "/workspace/DATA/lc/lcrqagen_v3/g/69/traces/critiques/critique.jsonl"
 # output_dir = "/workspace/DATA/lc/lcrqagen_v3/g/69/traces/rewrites"
