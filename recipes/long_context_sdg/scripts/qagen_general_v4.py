d605f204c73c2228358c69140e26960ce522b505
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-16 18:30:13 -0800

    more v4

--- recipes/long_context_sdg/scripts/qagen_general_v4.py
+++ recipes/long_context_sdg/scripts/qagen_general_v4.py
@@ -20,8 +20,8 @@ output_dir = "/workspace/DATA/lc/lcrqagen_v4/s/general"
 # 3input_file = "/workspace/DATA/lcr_docs/Company_Documents-16384-130000.jsonl"
 # output_dir = "/workspace/DATA/lc/lcrqagen_v4/c/general"
 
-# input_file = "/workspace/DATA/lcr_docs/Government_Consultations-16384-130000.jsonl"
-# output_dir = "/workspace/DATA/lc/lcrqagen_v4/g/general"
+input_file = "/workspace/DATA/lcr_docs/Government_Consultations-16384-130000.jsonl"
+output_dir = "/workspace/DATA/lc/lcrqagen_v4/g/general"
 
 
 prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_qa_general_v4.yaml"
