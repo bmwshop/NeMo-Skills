b7f7ebd3109bd3def959124c2f1bdfecd238128b
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-17 20:24:55 -0800

    more traces

--- recipes/long_context_sdg/scripts/gentrace.py
+++ recipes/long_context_sdg/scripts/gentrace.py
@@ -24,8 +24,8 @@ output_dir = "/workspace/DATA/lc/lcrqagen_v4/i/general/traces_b"
 input_file = "/workspace/DATA/lc/lcrqagen_v4/l/general/intermediate_p.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen_v4/l/general/traces_b"
 
-# input_file = "/workspace/DATA/lc/lcrqagen_v4/s/general/intermediate_p.jsonl"
-# output_dir = "/workspace/DATA/lc/lcrqagen_v4/s/general/traces_b"
+input_file = "/workspace/DATA/lc/lcrqagen_v4/s/general/intermediate_p.jsonl"
+output_dir = "/workspace/DATA/lc/lcrqagen_v4/s/general/traces_b"
 
 # input_file = "/workspace/DATA/lc/lcrqagen_v4/g/general/intermediate_p.jsonl"
 # output_dir = "/workspace/DATA/lc/lcrqagen_v4/g/general/traces_b"
