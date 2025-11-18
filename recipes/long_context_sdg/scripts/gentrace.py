262475672039caee36b262f4e69fccb6af553509
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-17 20:26:16 -0800

    more traces

--- recipes/long_context_sdg/scripts/gentrace.py
+++ recipes/long_context_sdg/scripts/gentrace.py
@@ -27,8 +27,8 @@ output_dir = "/workspace/DATA/lc/lcrqagen_v4/l/general/traces_b"
 input_file = "/workspace/DATA/lc/lcrqagen_v4/s/general/intermediate_p.jsonl"
 output_dir = "/workspace/DATA/lc/lcrqagen_v4/s/general/traces_b"
 
-# input_file = "/workspace/DATA/lc/lcrqagen_v4/g/general/intermediate_p.jsonl"
-# output_dir = "/workspace/DATA/lc/lcrqagen_v4/g/general/traces_b"
+input_file = "/workspace/DATA/lc/lcrqagen_v4/g/general/intermediate_p.jsonl"
+output_dir = "/workspace/DATA/lc/lcrqagen_v4/g/general/traces_b"
 
 
 # prompt_config = "/nemo_run/code/recipes/long_context_sdg/prompts/gen_trace.yaml"
