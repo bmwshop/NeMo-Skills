b8cfe39e26f66801bac18ff17b718c821fc2b9a6
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-20 17:33:43 -0800

    v3a

--- recipes/long_context_sdg/scripts/qagen_v3args.py
+++ recipes/long_context_sdg/scripts/qagen_v3args.py
@@ -17,10 +17,10 @@ prompt_config = f"/nemo_run/code/recipes/long_context_sdg/prompts/v3a/gen_qa_{te
 # teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
 teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Thinking-2507"
 
-if not os.path.exists(prompt_config):
-    # raise FileNotFoundError(f"Prompt configuration file not found: {prompt_config}")
-    print(f"Prompt configuration file not found: {prompt_config}")
-    exit(1)
+# if not os.path.exists(prompt_config):
+#     # raise FileNotFoundError(f"Prompt configuration file not found: {prompt_config}")
+#     print(f"Prompt configuration file not found: {prompt_config}")
+#     exit(1)
 
 dependent_jobs = 2
 num_servers = 1
