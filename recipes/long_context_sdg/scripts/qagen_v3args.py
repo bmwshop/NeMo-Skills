cdc9429484cbea48074c263a2cb0dfdc619de6a6
Dima Rekesh <drekesh@oci-hsg-cs-001-login-03.cm.cluster>
2025-11-20 17:30:34 -0800

    v3a

--- recipes/long_context_sdg/scripts/qagen_v3args.py
+++ recipes/long_context_sdg/scripts/qagen_v3args.py
@@ -13,7 +13,7 @@ cluster = "hsg"
 # input_file = "/nemo_run/code/recipes/long_context_sdg/src/test.jsonl"
 input_file = "/workspace/DATA/lcr_docs/acgilms-16384-130000.jsonl" # all v3 files
 output_dir = f"/workspace/DATA/lc/lcrqagen_v3a/{template}"
-prompt_config = f"/nemo_run/code/recipes/long_context_sdg/prompts/v3a/gen_qa_{template}_v3.yaml"
+prompt_config = f"/nemo_run/code/recipes/long_context_sdg/prompts/v3a/gen_qa_{template}.yaml"
 # teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Instruct-2507"
 teacher_model = "/hf_models/Qwen_Qwen3-235B-A22B-Thinking-2507"
 
