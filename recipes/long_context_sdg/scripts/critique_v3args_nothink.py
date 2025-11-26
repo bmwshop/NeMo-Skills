d511350fbfc16916e72fbf24d6c06561c6c09e13
Dima Rekesh <drekesh@oci-hsg-cs-001-login-02.cm.cluster>
2025-11-26 09:14:47 -0800

    tweaks to v3b

--- recipes/long_context_sdg/scripts/critique_v3args_nothink.py
+++ recipes/long_context_sdg/scripts/critique_v3args_nothink.py
@@ -56,7 +56,7 @@ generate(
     server_args=f"--context-len {MAX_SEQ_LEN} --ep-size 8",
     num_chunks=num_servers,
     dependent_jobs=dependent_jobs,
-    postprocess_cmd=f"python /nemo_run/code/recipes/long_context_sdg/scripts/postprocess_qa.py {output_dir}/output.jsonl {output_dir}/critique.jsonl",
+    postprocess_cmd=f"python /nemo_run/code/recipes/long_context_sdg/scripts/postprocess_qa.py --input {output_dir}/output.jsonl --output {output_dir}/critique.jsonl",
     server_gpus=4,
     server_nodes=2,
 )
